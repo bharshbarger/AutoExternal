9ffe3d55a47221aaed67245b918578ee6a46fac3
Bill Harshbarger <bill.harshbarger@gmail.com>
2017-03-17 03:46:22 -0500

    initial functions: nmap, whois, rev dns, input file or ip as arg and validate

--- AutoExt.py
+++ AutoExt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 #flow:
 #get scope, test IP for valid IP
@@ -15,7 +15,7 @@
 
 
 #ssl
-#https://github.com/nabla-c0d3/sslyze
+
 
 
 
@@ -30,79 +30,168 @@
 #create some sort of findings db with like flask or tornado?
 
 try:
-	import libnmap, sslyze, whois
+	#builtins
+	import argparse, time, os, sys, ftplib, socket, subprocess
+
+	#local imports
+	from reportgen import Reportgen
+
+	#dependencies
+	import libnmap, ipwhois
+
 	from libnmap.process import NmapProcess
-	import os, ftplib, time
-
+	
 except Exception as e:
-	print('\n [!] Imports failed! ' +str(e))
-	sys.exit(1)
+	print('\n [!] Failed imports: ' +str(e))
 
 
-class AutoExt():
-	def __init__(self):
+class AutoExt:
+	def __init__(self, args):
 		self.version ='A1.031717'
+		
+		#start timer
+		self.startTime=time.time()
+
+		#local dirs
+		self.reportDir='./reports/'
+
+		#check local dirs
+		if not os.path.exists(self.reportDir):
+			os.makedirs(self.reportDir)
+		self.targetsFile = ''
+		self.targets=[]
+
+	def clear(self):
+
+		#clean up screen
+	    os.system('cls' if os.name == 'nt' else 'clear')
 
 
-	def parsenmap(self):
+	def banner(self, args):
+			
+		#verbosity flag to print logo and args
+		if args.verbose is True:print( '''
+    _         _        _____      _                        _ 
+   / \  _   _| |_ ___ | ____|_  _| |_ ___ _ __ _ __   __ _| |
+  / _ \| | | | __/ _ \|  _| \ \/ / __/ _ \ '__| '_ \ / _` | |
+ / ___ \ |_| | || (_) | |___ >  <| ||  __/ |  | | | | (_| | |
+/_/   \_\__,_|\__\___/|_____/_/\_/\__\___|_|  |_| |_|\__,_|_|\n''')
+
+		if args.verbose is True:print ('AutoExternal.py %s, a way to automate common external testing tasks\n' % self.version)
+		if args.verbose is True:print (args)
 
 
-	#https://pypi.python.org/pypi/whois
-	def whois(self):
-		domain = whois.query(host)
-		print(domain.__dict__)
-		print(domain.name)
-		print(domain.expiration_date)
+	
+	def checkargs(self, args, parser):
+
+		#require at least one argument
+		if not (args.file or args.ipaddress):
+		    print('\n[!] No scope provided, add a file with IPs with -f or IP address(es) with -i\n')
+		    parser.print_help()
+		    sys.exit(1)
+
+		if args.file is not None and args.ipaddress is None:
+			print('[i] Opening targets file')
+
+			self.targetsFile=args.file
+
+			with open(self.targetsFile) as f:
+				targets = f.readlines()
+				targets = [x.strip() for x in targets]
+				self.targets = targets
+				for i,t in enumerate(self.targets):
+				
+					try:
+						socket.inet_aton(str(t))
+					except socket.error:
+						print ('[!] Invalid IP address %s entered at line %s!' %  (t,i+1))
+						sys.exit()
+
+
+
+				print('[+] All target IP addresses are valid!')
+
+		if args.threads is None:
+			args.threads = 2
+
+		if args.threads is not None:
+			args.threads=int(args.threads)
+
+	#run the docx report. text files happen in the respective functions
+	def report(self, args):
+		
+		reportGen = Reportgen()
+		reportGen.run(args, self.reportDir, self.lookup, self.whoisResult, self.domainResult, self.googleResult, self.shodanResult, self.pasteScrapeResult, self.harvesterResult, self.scrapeResult, self.credResult, self.pyfocaResult)
+
+
+	#https://ipwhois.readthedocs.io/en/latest/NIR.html
+	def whois(self, args):
+		whoisResult=set()
+
+
+		for t in self.targets:
+			try:
+				subprocess.Popen(['whois',t], stdout = subprocess.PIPE).communicate()[0].split('\n')
+			except:
+				print '[-] Error running whois command'
+				sys.exit()
+			time.sleep(5)
+
+	def dnslookup(self,args):
+		domainResult=set()
+
+		for t in self.targets:
+
+			domain=(socket.gethostbyaddr(t)[0].split('.')[1:])
+			domainResult.add('.'.join(domain))
+			print socket.gethostbyaddr(t)[0]
+			print domainResult
+
+			#domainResult.add(socket.gethostbyaddr(t))
+			time.sleep(1)
 
 	#https://libnmap.readthedocs.io/en/latest/process.html
-	def nmap(self):
+	def nmap(self, args):
 
-		nm = NmapProcess("scanme.nmap.org", options="-sV")
-		rc = nm.run()
+		print ('[i] Running nmap scan against %s targets' % len(self.targets))
 
-		if nm.rc == 0:
-			print nm.stdout
-		else:
-			print nm.stderr
+		nmap_proc = NmapProcess(targets=self.targets, options="-n -p- -T4 --min-hostgroup=50")
+		nmap_proc.run_background()
+		while nmap_proc.is_running():
+		    print("Nmap Scan running: ETC: {0} DONE: {1}%".format(nmap_proc.etc,
+		                                                          nmap_proc.progress))
+		    time.sleep(10)
 
-	#https://docs.python.org/3.6/library/ftplib.html
-	def ftpscan(self, host):
-		try:
-			ftpConn = ftplib.FTP(host)
-			ftpConn.login('anonymous', 'me@your.com')
-			print('\n[*] ' + str(host) + ' FTP Anonymous Login Found.')
-			ftpConn.quit()
-		except Exception as e:
-			print('\n[*] ' + str(host) + ' FTP Anonymous Login Failed.')
+		print("rc: {0} output: {1}".format(nmap_proc.rc, nmap_proc.summary))
+
+
 
 def main():
 
 	#https://docs.python.org/3/library/argparse.html
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-a', '--all', help = 'run All queries', action = 'store_true')
-	parser.add_argument('-b', '--hibp', help='Search haveibeenpwned.com for breaches related to a domain', action='store_true')
-	parser.add_argument('-c', '--creds', help = 'Search local copies of credential dumps', action = 'store_true')
-	parser.add_argument('-d', '--domain', metavar='foo.com', nargs = 1, help = 'the Domain you want to search.')
+	parser.add_argument('-c', '--client', help = 'client name')
 	parser.add_argument('-f', '--file', metavar='targets.txt',help = 'input file')
-	parser.add_argument('-g', '--googledork', metavar='password id_rsa', nargs = '+',help = 'query Google for supplied args that are treated as a dork. i.e. -g password becomes a search for "password site:<domain>". Combine terms inside of quotes like "site:rapid7.com inurl:aspx" ')
-	parser.add_argument('-i', '--ipaddress', nargs = 1, help = 'the IP address you want to search. Must be a valid IP. ')
-	#parser.add_argument('-n', '--nslookup',help = 'Name query DNS for supplied -d or -i values. Requires a -d or -i value', action = 'store_true')
-	#parser.add_argument('-p', '--pastebinsearch', metavar='password id_rsa' ,nargs = '+', help = 'Search google for <arg> site:pastebin.com. Requires a pro account if you dont want to get blacklisted.')
-	parser.add_argument('-s', '--shodan', help = 'query Shodan, API keys stored in ./api_keys/', action='store_true')
-	parser.add_argument('-S', '--scraper', help = 'Scrape pastebin, github, indeed, more to be added. API keys stored in ./api_keys/', action = 'store_true')
-	#parser.add_argument('-t', '--theharvester', help = 'Invoke theHarvester', action = 'store_true')
+	parser.add_argument('-i', '--ipaddress', metavar='127.0.0.1',help = 'IP address(es) to scan')
+	parser.add_argument('-n', '--nmap', metavar='nmap options',help = 'run nmap')
+	parser.add_argument('-t', '--threads', metavar='2', help='generally how parallel to run tests')
 	parser.add_argument('-v', '--verbose', help = 'Verbose', action = 'store_true')	
-	#parser.add_argument('-w', '--whois', help = 'query Whois for supplied -d or -i values. Requires a -d or -i value', action = 'store_true')
-		
+	
 	args = parser.parse_args()
 
 	#run functions with arguments passed
-	runAutosint = Autosint(args)
-	runAutosint.clear()
-	runAutosint.banner(args)
-	runAutosint.checkargs(args)
-	runAutosint.report(args)
-	
+	runAutoext = AutoExt(args)
+	runAutoext.clear()
+	runAutoext.banner(args)
+	runAutoext.checkargs(args, parser)
+	#runAutoext.dnslookup(args)
+	runAutoext.whois(args)
+	#runAutoext.nmap(args)
+	#runAutoext.ftp(args)
+
+	#runAutoext.report(args)
+
 if __name__ == '__main__':
 
 	main()
