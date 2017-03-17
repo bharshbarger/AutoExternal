11e6fa25d547db9f7d6eda20a3693e023ce8a8e2
bharshbarger <bharshbarger@users.noreply.github.com>
2017-03-17 00:18:52 -0500

    Update AutoExt.py

--- AutoExt.py
+++ AutoExt.py
@@ -3,8 +3,43 @@
 #flow:
 #import and run autosint, linkscrape, smbshakedown
 #do an nmap, save to db?
-#convert output for use in sslscan
-#if udp500, ike-scan/ikeforce
+#https://pypi.python.org/pypi/python-libnmap/0.6.1
+#pip install libnmap
+#store nmap to a db
+
+
+#ssl
+#https://github.com/nabla-c0d3/sslyze
+
+
+
+
+#if udp500, ike-scan/ikeforce/iker
+#https://labs.portcullis.co.uk/tools/iker/
+
+#ftp anon login or spray with ftplib
+
 #if 80/443 scrape for login
 #repurpose docx reportgen for report
 #create some sort of findings db with like flask or tornado?
+
+
+
+import libnmap, sslyze
+import os, ftplib
+
+
+def ftpscan(host):
+  	try:
+      ftpConn = ftplib.FTP(host)
+      ftpConn.login('anonymous', 'me@your.com')
+      print('\n[*] ' + str(host) + ' FTP Anonymous Login Found.')
+      ftpConn.quit()
+
+	  except Exception as e:
+      print('\n[*] ' + str(host) + ' FTP Anonymous Login Failed.')
+
+
+
+if __name__ == '__main__':
+	main()
