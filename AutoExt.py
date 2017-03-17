4dc1e798933829426a60d125a4d8cd4e6d18d52f
bharshbarger <bharshbarger@users.noreply.github.com>
2017-03-16 23:39:18 -0500

    Update AutoExt.py

--- AutoExt.py
+++ AutoExt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 #flow:
+#import and run autosint, linkscrape, smbshakedown
 #do an nmap, save to db?
 #convert output for use in sslscan
 #if udp500, ike-scan/ikeforce
-#
+#if 80/443 scrape for login
