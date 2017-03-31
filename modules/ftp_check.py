6ef4e68d4aa0809604563145f578a790d0d22f68
Bill Harshbarger <bill.harshbarger@gmail.com>
2017-03-31 02:51:37 -0500

    add ftp check

---
+++ modules/ftp_check.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+from ftplib import FTP
+
+class Ftpscan:
+
+	def __init__():
+
+		self.target=''
+
+
+
+
+	def connect(self):
+		foo=''
+
+
+
+
+def main():
+
+	runFtp=Ftpscan()
+
+
+if __name__ == '__main__':
+	main()		
