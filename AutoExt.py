05817e4aaeee375ae7f01a8bc63a7a6c91837623
Bill Harshbarger <bill.harshbarger@gmail.com>
2017-03-31 02:34:53 -0500

    nmap experiments

--- AutoExt.py
+++ AutoExt.py
@@ -120,7 +120,7 @@ class AutoExt:
 			#if the ip isnt valid
 			except socket.error:
 				#tell them
-				if self.args.verbose is True:print ('[v] Invalid IP address [ %s ] found on line %s!' %  (t,i+1))
+				print ('[!] Invalid IP address [ %s ] found on line %s... Fixing!' %  (t,i+1))
 				
 				#fix the entries. this function will add resolved IPs to the targetSet
 				self.fix_targets(t)
@@ -223,7 +223,7 @@ def main():
 	runAutoext.banner()
 	runAutoext.checkargs()
 	runAutoext.add_client_db()
-	runAutoext.domainlookup()
+	#runAutoext.domainlookup()
 	runAutoext.nmap_scan()
 
 	#runAutoext.ftp(args)
--- modules/nmap.py
+++ modules/nmap.py
@@ -2,6 +2,7 @@
 
 
 try:
+	import time
 	from libnmap.process import NmapProcess
 	from libnmap.parser import NmapParser
 except Exception as e:
@@ -20,13 +21,45 @@ class AutoNmap:
 
 		print ('[i] Running nmap scan against %s targets\n' % len(self.targetList))
 
-		nm = NmapProcess(targets=self.targetList, options="-n -p80 -T4 --min-hostgroup=50")
-		nm.run()
+		nm = NmapProcess(targets=self.targetList, options="-n -p21,80,443,8080 -T4 --min-hostgroup=50")
+		rc=nm.run_background()
+
+		#if successful return code, dump the stdout xml of the nmap
+		if nm.rc == 0:
+			print nm.stdout
+		else:
+			print nm.stderr
+
+
+		while nm.is_running():
+			print("Nmap Scan running: ETC: {0} DONE: {1}%".format(nm.etc,nm.progress))
+			time.sleep(2)
+
+		print("rc: {0} output: {1}".format(nm.rc, nm.summary))
+
+
 
 		nmap_report = NmapParser.parse(nm.stdout)
+		return nmap_report
 		
-		for scanned_hosts in nmap_report.hosts:
-		    print scanned_hosts
+		#for scanned_hosts in nmap_report.hosts:
+			#print scanned_hosts
+		#self.print_scan(nmap_report)
+
+
+	def print_scan(self,nmap_report):
+		print("Starting Nmap {0} ( http://nmap.org ) at {1}".format(
+			nmap_report.version,
+			nmap_report.started))
+		for host in nmap_report.hosts:
+			if len(host.hostnames):
+				tmp_host = host.hostnames.pop()
+			else:
+				tmp_host = host.address
+			print("Nmap scan report for {0} ({1})".format(
+				tmp_host,
+				host.address))
+
 
 	#https://libnmap.readthedocs.io/en/latest/process.html
 	def scan_udp(self):
