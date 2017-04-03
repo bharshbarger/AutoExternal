14a0b24ecac8bd4f5f27149e1bc5c19a70b8afe7
Bill Harshbarger <bill.harshbarger@gmail.com>
2017-04-03 16:56:39 -0500

    set up defs for domain and nmap db commits

--- modules/dbcommands.py
+++ modules/dbcommands.py
@@ -63,6 +63,94 @@ class Database:
 		print('\n')
 
 
+
+
+	def add_domains(self):
+
+		dbconn=self.connect()
+
+		#conn to db
+		cur = dbconn.cursor()
+		print('[i] Adding client [ %s ] to database:' % self.clientName)
+		c=self.clientName
+		#insert rows
+		print('ID___ Name___________ Contact____________  Date_______________')
+
+
+		#check to see if the client name exists, and if it does print it, and if it doesnt add it
+		try:
+			#look for existing name from supplied arg
+			cur.execute("SELECT * FROM client WHERE (name = '%s') " % (c))
+			results = cur.fetchall()
+			cur.close()
+
+			#if there is a result
+			if results is not None:
+				#print it
+				for row in results:
+					print ('%-5s %-15s %-20s %-s' % (row[0], row[1], row[2], row[3]))
+			#if there isn't a result
+			else:
+				#add customer
+				try:
+					cur.execute("INSERT INTO client (name) VALUES ('%s') " % (c))
+					dbconn.commit()
+					#and display it
+					cur.execute("SELECT * FROM client WHERE (name = '%s') " % (c))
+					results = cur.fetchall()
+					cur.close()
+				except sqlite3.Error as e:
+					print("[-] Database Error: %s" % e.args[0])
+
+		except sqlite3.Error as e:
+			print("[-] Database Error: %s" % e.args[0])
+
+		print('\n')
+
+
+
+	def add_nmap(self):
+
+		dbconn=self.connect()
+
+		#conn to db
+		cur = dbconn.cursor()
+		print('[i] Adding host [ %s ] to database:' % self.clientName)
+		c=self.clientName
+		#insert rows
+		print('ID___ Name___________ Contact____________  Date_______________')
+
+
+		#check to see if the client name exists, and if it does print it, and if it doesnt add it
+		try:
+			#look for existing name from supplied arg
+			cur.execute("SELECT * FROM client WHERE (name = '%s') " % (c))
+			results = cur.fetchall()
+			cur.close()
+
+			#if there is a result
+			if results is not None:
+				#print it
+				for row in results:
+					print ('%-5s %-15s %-20s %-s' % (row[0], row[1], row[2], row[3]))
+			#if there isn't a result
+			else:
+				#add customer
+				try:
+					cur.execute("INSERT INTO client (name) VALUES ('%s') " % (c))
+					dbconn.commit()
+					#and display it
+					cur.execute("SELECT * FROM client WHERE (name = '%s') " % (c))
+					results = cur.fetchall()
+					cur.close()
+				except sqlite3.Error as e:
+					print("[-] Database Error: %s" % e.args[0])
+
+		except sqlite3.Error as e:
+			print("[-] Database Error: %s" % e.args[0])
+
+		print('\n')
+
 def main():
 
 	dbOps=Database()
